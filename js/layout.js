f8694a4913cc2cf041eb6486075ee993870c9d51
Emeka Orji <emekapraiseo@gmail.com>
2022-03-10 13:07:04 +0100

    Finished with search-bar styling

---
+++ js/layout.js
@@ -0,0 +1,12 @@
+const searchBarBox = document.getElementById('searchBar');
+const searchBar = document.getElementById('searchBarInput');
+
+searchBar.addEventListener('focus', () => {
+	searchBarBox.classList.add('search-bar_active');
+});
+searchBar.addEventListener('blur', () => {
+	searchBarBox.classList.remove('search-bar_active');
+});
+
+console.log(searchBar);
+console.log(searchBarBox);
